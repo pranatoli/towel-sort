246a665d348235721b9bfe6662bde840c4f1b331
pranatoli <pr.anatoli@gmail.com>
2022-07-31 20:08:23 +0300

    init: make excercise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (typeof (matrix) == 'undefined') { return [] };
+  const arr = [...matrix];
+  matrix.forEach((element, index) => {
+    if (index % 2 != 0) {
+      element.reverse();
+    }
+  });
+  return arr.flat();
 }
+
+
